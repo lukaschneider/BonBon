3a7d4dadf3fa6a1307515ad3f3130c576f1b0705
Lukas Schneider <schneiderlukas1999@gmail.com>
2022-11-25 09:38:09 +0100

    chore: change bons and prices for christmas market

--- lib/model/bon.dart
+++ lib/model/bon.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class BonModel extends ChangeNotifier {
   List<Bon> _availableBons = [
-    Bon(Colors.red.shade500, 'Limo', 2.00),
-    Bon(Colors.pink.shade100, 'Pinktwenty', 2.80),
-    Bon(Colors.yellow.shade600, 'Pommes', 3.00),
-    Bon(Colors.grey.shade200, 'Waffel', 1.50),
-    Bon(Colors.blue.shade300, 'Bier/Radler, Kaffee, Wein/Wein gesprizt', 2.50),
+    Bon(Colors.red.shade500, 'Glühwein', 3.00),
+    Bon(Colors.pink.shade100, 'Tee mit Rum', 3.00),
+    Bon(Colors.yellow.shade600, 'Tee', 2.00),
+    Bon(Colors.green.shade200, 'Bratapfellikör mit Sahne', 3.50),
+    Bon(Colors.blue.shade300, 'Waffel', 2.00),
+    Bon(Colors.purple.shade300, 'Waffel mit Sahne', 2.50),
+    Bon(Colors.grey.shade300, 'Tassenpfand', 2.00),
   ];
 
   List<Bon> get availableBons => _availableBons;
